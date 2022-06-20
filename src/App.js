41b18af6ef416f7ff6313f2f9804b17d5aad6106
Soner Şimşek <sonersimsekhd@gmail.com>
2022-06-20 21:10:04 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Dash from "./components/Dash";
+import Login from "./components/Login";
+import "./App.css"
+function App() {
+  return (
+    <div className="app">
+      <div><Dash/></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+export default function Login() {
+  return (
+    <>
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Username" />
+          <Form.Text className="text-muted">
+            We'll never share your username with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </div>
+    </>
+  );
+}
